891cfd85e59b7333dfbd023e63d7482193ef2d2d
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-07-22 12:15:40 -0300

    chore: add styling to selected language

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,20 +1,38 @@
 <template>
   <div class="navigation">
-    <a href="#about">{{$t('About')}}</a>
-    <a href="#skills">{{$t('Skills')}}</a>
-    <a href="#contact">{{$t('Contact')}}</a>
+    <ul class="navigation-links">
+      <li><a href="#about">{{$t('About')}}</a></li>
+      <li><a href="#skills">{{$t('Skills')}}</a></li>
+      <!-- <li><a href="#skills">{{$t('Articles')}}</a></li> -->
+      <li><a href="#contact">{{$t('Contact')}}</a></li>
+    </ul>
+
     <div>
-      {{$t('language')}}: <span @click="$i18n.locale = $i18n.availableLocales[0]">Português</span>  | <span @click="$i18n.locale = $i18n.availableLocales[1]">English</span>
+      {{$t('language')}}: 
+      <span @click="$i18n.locale = $i18n.availableLocales[0]" :class="{ active: selectedLanguage }">Português</span>  
+      | 
+      <span @click="$i18n.locale = $i18n.availableLocales[1]" :class="{ active: !selectedLanguage }">English</span>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "Navigation",
+  computed: {
+    selectedLanguage() {
+      return this.$i18n.locale === this.$i18n.availableLocales[0] ? true : false;
+    },
+  },
+};
+</script>
 
 <style lang="scss">
   .navigation{
     width: 100vw;
     max-width: 100%;
-    height: 50px;
-    padding: 0 15px;
+    height: auto;
+    padding: 15px 15px;
 
     background-color: #2E5897;
 
@@ -25,19 +43,34 @@
     justify-content: flex-end;
     grid-gap: 25px;
 
-    a{
+    a {
+      font-size: clamp(1.2rem, 2.5vw, 1.5rem);
       text-decoration: none;
       color: #eee;
-      padding: 10px;
+      padding: 0 20px;
+      white-space: nowrap;
     }
 
     span {
       cursor: pointer;
     }
+
+    .navigation-links {
+      list-style-type: none;
+      list-style-position: inside;
+      display: flex;
+    }
+  }
+
+  .active {
+    background-color: #F4796B;
+    border-radius: 8px;
+    padding: 5px;
   }
 
   @media (max-width: 800px){
   .navigation{
+    flex-direction: column;
     justify-content: center;
   }
 
